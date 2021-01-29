6459e646cb3856ea54460d78db2aa3ea43aeea5b
Bermarte <bernardo.martelli@gmail.com>
2021-01-29 05:32:36 +0100

    Todo class and object creation plus delete single todo

---
+++ src/classes/Todo.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//todo class
+export default class Todo {
+    constructor(id, title, description){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        //build the UI
+        const tr = document.createElement('tr');
+        const tdTitle = document.createElement('td');
+        tdTitle.innerHTML = this.title;
+        tr.appendChild(tdTitle);
+        const tdDescription = document.createElement('td');
+        tdDescription.innerHTML = this.description;
+        tr.appendChild(tdDescription);
+        const closeBtn = document.createElement('a');
+        closeBtn.classList.add('btn', 'btn-danger', 'btn-sm', 'delete', 'end');
+        //remove the UI element
+        const toRemove = 'this.parentNode.parentNode.removeChild(this.parentNode)';
+        closeBtn.setAttribute('onclick',toRemove);
+        closeBtn.innerHTML = 'X';
+        tr.appendChild(closeBtn);
+        return tr;
+    };
+    setDescription(text){
+        this.description = text;
+    };
+    setTitle(text){
+        this.title = text;
+    };
+}
