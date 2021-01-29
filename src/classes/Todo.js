86fe0c3e73da2503ec76afd9236055ea15aad951
Tahmina <thm.rasoli@gmail.com>
2021-01-29 21:33:37 +0430

    form validation checks if the field is empty

--- src/classes/Todo.js
+++ src/classes/Todo.js
@@ -1,6 +1,10 @@
 'use strict';
 
 //todo class
+
+
+
+
 export default class Todo {
     constructor(id, title, description){
         this.id = id;
@@ -21,8 +25,19 @@ export default class Todo {
         closeBtn.setAttribute('onclick',toRemove);
         closeBtn.innerHTML = 'X';
         tr.appendChild(closeBtn);
+        tr.setAttribute('contenteditable', true);
         return tr;
-    };
+    }
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.form-parent');
+        const form = document.querySelector('#todo-form');
+       // console.log('form parent elements: '+ form.parentElement.parentElement.classList)
+        container.insertBefore(div, form);
+        setTimeout(() => document.querySelector('.alert').remove(), 3000)
+    }
     setDescription(text){
         this.description = text;
     };
@@ -30,3 +45,4 @@ export default class Todo {
         this.title = text;
     };
 }
+
