8dda22e533d6c37377bd39f5823012940e1ce485
Bermarte <bernardo.martelli@gmail.com>
2021-02-01 17:32:02 +0100

    cleaning up

--- src/classes/Validator.js
+++ src/classes/Validator.js
@@ -16,18 +16,6 @@ export default class Validator {
    * @param  {string} className - a CSS class
    * 
    */
-  // with a static method we cannot instantiate the class, insteed we have to call it with the class its self.
-  // static showAlert(message, className){
-  //       const div = document.createElement('div');
-  //       div.className = `alert alert-${className}`;
-  //       div.appendChild(document.createTextNode(message));
-  //       const container = document.querySelector('.form-parent');
-  //       const form = document.querySelector('#todo-form');
-  //      // console.log('form parent elements: '+ form.parentElement.parentElement.classList)
-  //       container.insertBefore(div, form);
-  //       setTimeout(() => document.querySelector('.alert').remove(), 3000)
-  //   }
-
   showAlert(message, className){
           const div = document.createElement('div');
           div.className = `alert alert-${className}`;
