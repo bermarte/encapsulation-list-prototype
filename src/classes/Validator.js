1b8967e5ec2c8ff1264a5f068d95e1e15ffade70
Tahmina <thm.rasoli@gmail.com>
2021-02-01 16:44:33 +0430

    applied comments

--- src/classes/Validator.js
+++ src/classes/Validator.js
@@ -12,22 +12,10 @@ export default class Validator {
   /**
    * showAlert method will show an alert message when called
    * 
-   * @param  {string} message - a text message
+   * @param  {string} errMessage - a text message
    * @param  {string} className - a CSS class
    * 
    */
-  // with a static method we cannot instantiate the class, insteed we have to call it with the class its self.
-  // static showAlert(message, className){
-  //       const div = document.createElement('div');
-  //       div.className = `alert alert-${className}`;
-  //       div.appendChild(document.createTextNode(message));
-  //       const container = document.querySelector('.form-parent');
-  //       const form = document.querySelector('#todo-form');
-  //      // console.log('form parent elements: '+ form.parentElement.parentElement.classList)
-  //       container.insertBefore(div, form);
-  //       setTimeout(() => document.querySelector('.alert').remove(), 3000)
-  //   }
-
   showAlert(message, className){
           const div = document.createElement('div');
           div.className = `alert alert-${className}`;
